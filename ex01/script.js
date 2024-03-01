4f09f303748bcde3bc82d1942cdb3cdd65a8d856
alexandrerosapc <alexandrerosasoares3@gmail.com>
2024-03-01 13:07:04 -0300

    Exercícios de JS

---
+++ ex01/script.js
@@ -0,0 +1,6 @@
+let nome = prompt('Qual seu nome?')
+let n1 = prompt('Escolha um número?')
+let n2 = prompt('Escolha outro número?')
+const soma = Number(n1) + Number(n2)
+alert(`É um grande prazer em te conhecer ${nome}!
+E a sua soma é ${soma}`)
